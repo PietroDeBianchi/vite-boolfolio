ebbabdb1e5220f3daf1edeb9c163c4442dd9fbb4
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-06-09 15:51:38 +0200

    multi page

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -26,10 +26,12 @@
             <nav aria-label="Page navigation example">
                 <ul class="pagination">
                     <li class="page-item me-2">
-                        <button @click="getProject(currentPage - 1)" class="btn btn-primary previous">&laquo;</button>
+                        <button @click="getProject(currentPage - 1)" class="btn btn-primary previous"
+                            :class="{ 'disabled': currentPage == 1 }">&laquo;</button>
                     </li>
                     <li class="page-item me-2">
-                        <button @click="getProject(currentPage + 1)" class="btn btn-primary next">&raquo;</button>
+                        <button @click="getProject(currentPage + 1)" class="btn btn-primary next"
+                            :class="{ 'disabled': currentPage == lastPage }">&raquo;</button>
                     </li>
                 </ul>
             </nav>
@@ -45,29 +47,35 @@ export default {
 
     data() {
         return {
-            projects: [],
-            baseUrl: 'http://127.0.0.1:8000',
-            currentPage: 1
+            projects: [], // Array to store the project data
+            baseUrl: 'http://127.0.0.1:8000', // Base URL for API requests
+            currentPage: 1, // Current page of projects
+            lastPage: null // Last page of projects
         }
     },
     methods: {
         getProject(pageScroll) {
+            // Method to fetch project data from the API based on the page
             axios.get(`${this.baseUrl}/api/projects`, {
                 params: {
-                    page: pageScroll // don't forget this.
+                    page: pageScroll // Specify the page to fetch !dont forget!
                 }
             })
                 .then(response => {
+                    // Update the project data, current page, and last page based on the API response
                     this.projects = response.data.results.data;
                     this.currentPage = response.data.results.current_page;
-                    console.log(response);
+                    this.lastPage = response.data.results.last_page;
+                    console.log(response); // Log the API response for debugging purposes
                 })
         }
     },
     mounted() {
+        // Call the getProject method with the initial page (1) when the component is mounted
         this.getProject(1);
     }
 }
 </script>
+
 
 <style lang="scss"></style>
